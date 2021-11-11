45aa87c9c9795923f5611e4ed522b0b4d11f984e
arminv1001 <arminvosoghi@gmail.com>
2021-11-11 23:05:51 +0100

    Broker

---
+++ Broker.py
@@ -0,0 +1,23 @@
+class Broker:
+    ## Abstrakte Klasse
+    def __init__(self,BrokerName,ApiUrl):
+        self.__broker_name = BrokerName
+        self.__api_url = ApiUrl
+    
+    def getBrokerName(self):
+        return self.__broker_name
+    
+    def sendOrder(self,price,stoploss,takeprofit):
+        return "Need to be implemented"
+    
+    def sendTrade(self):
+        return "Need to be implemented"
+    
+    def closeTrade(self,id):
+        return "Need to be implemented"
+    
+    def closeOrder(self,id):
+        return "Need to be implemented"
+
+    def getHistoricalData(self,start_date,end_date):
+        return "Need to be implemented"
